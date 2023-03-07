5c55e6f3bd2000d61b5258bab885a68e131ec2a0
Andrey Kim <akart.dev@gmail.com>
2023-03-07 16:11:32 +0600

    feat: add reactive form

--- medium-frontend/src/app/auth/components/register/register.component.ts
+++ medium-frontend/src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 
 @Component({
@@ -7,7 +8,27 @@ import { Component } from "@angular/core";
   styleUrls: ['./register.component.scss']
 })
 
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: '',
+      password: ''
+    })
+  }
+
+  onSubmit():void {
+    console.log(this.form.value)
+  }
 }
 
