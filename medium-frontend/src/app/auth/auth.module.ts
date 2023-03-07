48d88df9648bbab57343d99fe9b77bd155d2f2d7
Andrey Kim <akart.dev@gmail.com>
2023-03-07 17:29:32 +0600

    feat: add ngrx

--- medium-frontend/src/app/auth/auth.module.ts
+++ medium-frontend/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
 
 import { RegisterComponent } from "src/app/auth/components/register/register.component";
 
@@ -13,7 +14,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule, StoreModule.forRoot({})],
   declarations: [RegisterComponent]
 
 })
--- medium-frontend/src/app/auth/components/register/register.component.ts
+++ medium-frontend/src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { registerAction } from "../../store/actions/register.action";
 
 
 @Component({
@@ -11,7 +13,7 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterComponent implements OnInit{
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private store: Store) {
 
   }
 
@@ -29,6 +31,7 @@ export class RegisterComponent implements OnInit{
 
   onSubmit():void {
     console.log(this.form.value)
+    this.store.dispatch(registerAction(this.form.value))
   }
 }
 
